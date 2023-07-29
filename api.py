b4df973d806a2a93e31fab1f0d6184739bc8f3af
Mardhianto <riski.mardianto@gmail.com>
2023-07-29 22:50:38 +0700

    add list

--- api.py
+++ api.py
@@ -7,3 +7,23 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Application Delivered Successfully using pipeline!\n"
+
+@app.route('/dist/<path:req_path>')
+def dir_listing(req_path):
+    BASE_DIR = '/home/dicoding/simple-python-pyinstaller-app'
+
+    # Joining the base and the requested path
+    abs_path = os.path.join(BASE_DIR, req_path)
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+
+    # Show directory contents
+    files = os.listdir(abs_path)
+    return render_template('files.html', files=files)
+
--- jenkins/scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-set +x
-python -m flask --app api run &
-sleep 1
-echo $! > .pidfile

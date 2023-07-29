686c49830e6595d0379d3abfe000640a15e41547
Mardhianto <riski.mardianto@gmail.com>
2023-07-30 00:40:45 +0700

    add req

--- app.py
+++ app.py
@@ -15,3 +15,6 @@ def downloadFile():
        return send_file(path,mimetype='text/x-python', download_name='add2vals', as_attachment=True)
     except Exception as e:
         return str(e)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)

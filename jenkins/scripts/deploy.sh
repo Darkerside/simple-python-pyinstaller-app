d1e2facdec17c999e7f6985c5c53cb3e93c55331
Mardhianto <riski.mardianto@gmail.com>
2023-07-30 01:14:07 +0700

    fix build

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -8,4 +8,4 @@ ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'sudo su; cd dicoding
 
 echo ""
 echo "build the exec file"
-ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'sudo su; cd dicoding/simple-python-pyinstaller-app; pyinstaller -F add2vals.py'
+ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'sudo su; cd dicoding/simple-python-pyinstaller-app; pyinstaller -F ./sources/add2vals.py'

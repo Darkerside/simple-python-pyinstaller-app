a2e0a7860f3548fc779d8032d5d74a911b028f9b
Mardhianto <riski.mardianto@gmail.com>
2023-07-30 01:09:59 +0700

    add su

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,11 +1,11 @@
 echo ""
 echo "remove the old build first"
-ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'cd dicoding/simple-python-pyinstaller-app; rm -r -f dist build'
+ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'sudo su; cd dicoding/simple-python-pyinstaller-app; rm -r -f dist build'
 
 echo ""
 echo "pull the latest commit"
-ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'cd dicoding/simple-python-pyinstaller-app; git pull'
+ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'sudo su; cd dicoding/simple-python-pyinstaller-app; git pull'
 
 echo ""
 echo "build the exec file"
-ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'cd dicoding/simple-python-pyinstaller-app; pyinstaller -F add2vals.py'
+ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'sudo su; cd dicoding/simple-python-pyinstaller-app; pyinstaller -F add2vals.py'

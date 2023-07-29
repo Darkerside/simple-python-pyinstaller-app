bf62d463fb3b4c65d5e2acf25fd7da9d22d1b933
Mardhianto <riski.mardianto@gmail.com>
2023-07-29 23:19:41 +0700

    change deploy script

---
+++ jenkins/scripts/deploy.sh
@@ -0,0 +1,10 @@
+echo ""
+echo "remove the old build first"
+ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'cd dicoding/simple-python-pyinstaller-app; rm -r -f dist build'
+echo ""
+echo "pull the latest commit"
+ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'cd dicoding/simple-python-pyinstaller-app; git pull'
+
+echo ""
+echo "build the exec file"
+ssh -o StrictHostKeyChecking=no -l ec2-user 13.229.219.204 'cd dicoding/simple-python-pyinstaller-app; pyinstaller -F add2vals.py'
